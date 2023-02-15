bcbdc588fa1148c9007c24049a8cc57dae6f4c0e
rholibobo <rholipop@gmail.com>
2023-02-15 15:58:46 +0100

    Profile page func added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import { getAuth } from "firebase/auth";
+import React from "react";
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router";
 
 export default function Profile() {
+  const auth = getAuth()
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  function onLogout(){
+    auth.signOut()
+    navigate("/")
+  }
   return (
-    <div>Profile</div>
-  )
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center font-bold mt-6 ">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center ">Do you want to change your name?
+                <span className="text-red-600 hover:text-red-700 transition duration-150 ease-in-out ml-1 cursor-pointer">Edit</span>
+              </p>
+              <p onClick={onLogout} className="text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out cursor-pointer">Sign Out</p>
+            </div>
+
+            
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
